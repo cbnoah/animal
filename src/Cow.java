2ef5775d123e7a089c3a403c20c65a6d9c6fc1ac
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-01-08 11:36:44 +0100

    feat: eat func have been override for the cow class

--- src/Cow.java
+++ src/Cow.java
@@ -2,4 +2,9 @@ public class Cow extends Animal{
     public Cow(String name, String sound, int age) {
         super(name, sound, age);
     }
+
+    @Override
+    public void eat() {
+        System.out.println("The cow is eating grass.");
+    }
 }
