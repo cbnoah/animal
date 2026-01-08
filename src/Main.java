62d36fb5a02ba1c922f79c1e9cfeb63866a4e9c0
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-01-08 22:06:51 +0100

    feat: tests added

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,14 @@ public class Main {
         cow.eat("hay");
         cow.eat("ynov");
 
+        /* La classe Animal est abstraite, on ne peut pas l'instancier directement
         Animal genericAnimal = new Animal("Generic", "Sound", 1);
         genericAnimal.eat();
         genericAnimal.eat("food");
         System.out.println(genericAnimal.getAge());
         System.out.println(genericAnimal.getName());
         System.out.println(genericAnimal.getSound());
-
+         */
         dog.play();
 
         ArrayList<Pet> pets = new ArrayList<>();
@@ -36,5 +37,16 @@ public class Main {
         for (Pet pet : pets) {
             pet.play();
         }
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(cow);
+        animals.add(dog);
+
+        Owner owner = new Owner("Mathieu", animals);
+        owner.addAnimals(new Dog("Rex", "Woof", 2));
+        owner.addAnimals(new Cow("Daisy", "Moo", 4));
+
+        Cat cat = new Cat("Whiskers", "Meow", 2);
+        cat.hunt(dog);
     }
 }
